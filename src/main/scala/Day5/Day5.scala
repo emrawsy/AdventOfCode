388d2ccab6d4e1d3e8436cff0d79afe86a5cc9e5
emrawsy <27211889+emrawsy@users.noreply.github.com>
2022-12-30 11:36:01 +0000

    Day5- initial commit

---
+++ src/main/scala/Day5/Day5.scala
@@ -0,0 +1,12 @@
+package Day5
+
+import scala.io.Source
+
+object Day5 extends App {
+
+  val input = Source.fromResource("day5.txt").mkString
+
+
+
+  println()
+}
---
+++ src/test/scala/Day5/Day5Spec.scala
@@ -0,0 +1,19 @@
+package Day5
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+class Day5Spec extends AnyFreeSpec with Matchers {
+
+  private val x = ???
+
+  "task" - {
+    "part1" in {
+
+    }
+    "part2" in {
+
+    }
+}
+
+}
